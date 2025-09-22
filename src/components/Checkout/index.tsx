b8a28030d6a74ff5ae59910e6cadac62beed78a6
mahmudul165 <mahmudulh883@gmail.com>
2025-09-22 21:46:37 +0600

    add payment getway

--- src/components/Checkout/index.tsx
+++ src/components/Checkout/index.tsx
@@ -301,9 +301,9 @@ const Checkout = () => {
 
                 {/* Checkout Button */}
                 <button
-                 // type="submit"
+                 type="submit"
                   onClick={handlePayment}
-                  //disabled={loading}
+                  disabled={loading}
                   // className={`w-full flex justify-center font-medium text-white bg-blue py-3 px-6 rounded-md ease-out duration-200 hover:bg-blue-dark ${
                   //   loading ? "opacity-70 cursor-not-allowed" : ""
                   // }`}
