1335a1cc7146c833419a7637c12136805e3f1419
mahmudul165 <mahmudulh883@gmail.com>
2025-09-23 20:45:46 +0600

    added dynamically checkout page

--- src/app/(site)/(pages)/checkout/page.tsx
+++ src/app/(site)/(pages)/checkout/page.tsx
@@ -1,19 +1,41 @@
-import React from "react";
-import Checkout from "@/components/Checkout";
+// import React from "react";
+// import Checkout from "@/components/Checkout";
 
+// import { Metadata } from "next";
+// export const metadata: Metadata = {
+//   title: "Checkout Page | NextCommerce Nextjs E-commerce template",
+//   description: "This is Checkout Page for NextCommerce Template",
+//   // other metadata
+// };
+
+// const CheckoutPage = () => {
+//   return (
+//     <main>
+//       <Checkout />
+//     </main>
+//   );
+// };
+
+// export default CheckoutPage;
+ 
+
+import dynamic from "next/dynamic";
 import { Metadata } from "next";
+
 export const metadata: Metadata = {
   title: "Checkout Page | NextCommerce Nextjs E-commerce template",
   description: "This is Checkout Page for NextCommerce Template",
-  // other metadata
 };
 
-const CheckoutPage = () => {
+// Dynamic import without disabling SSR
+const Checkout = dynamic(() => import("@/components/Checkout"), {
+  loading: () => <p className="text-center py-10">Loading checkout...</p>,
+});
+
+export default function CheckoutPage() {
   return (
     <main>
       <Checkout />
     </main>
   );
-};
-
-export default CheckoutPage;
+}
